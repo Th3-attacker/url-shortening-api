046496f1244f78905fd3814f2dc130410001b25d
Th3-attacker <haje.malik5@gmail.com>
2024-01-10 15:10:00 +0000

    fix: :lipstick: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,75 @@
+import { useState } from "react";
 import logo from "../../public/images/logo.svg";
+import menu from "../../public/images/menu.svg";
+import close from "../../public/images/close.svg";
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  function handleClick() {
+    setIsOpen(!isOpen);
+  }
   return (
     <>
-      <header className="max-width ">
-        <div>
+      <header className="py-5 max-width">
+        <div className="flex items-center justify-between">
           <img src={logo} alt="" />
-          <nav className="">
-            <ul className="">
-              <li>
-                <a href="#">Features</a>
+          <div className="hidden space-x-14 md:flex">
+            <article className="flex items-center ">
+              <nav className="hidden md:block md:ml-5">
+                <ul className="flex items-start justify-start md:space-x-5">
+                  <li className="text-slate-400 hover:text-slate-800">
+                    <a href="#">Features</a>
+                  </li>
+                  <li className="my-5 md:my-0 text-slate-400 hover:text-slate-800">
+                    <a href="#">Pricing</a>
+                  </li>
+                  <li className="text-slate-400 hover:text-slate-800">
+                    <a href="#">Resources</a>
+                  </li>
+                </ul>
+              </nav>
+            </article>
+
+            <ul className="flex items-center justify-end space-x-5">
+              <li className="my-5 md:my-0 text-slate-400 hover:text-slate-800">
+                <button>Login</button>
               </li>
               <li>
-                <a href="#">Pricing</a>
-              </li>
-              <li>
-                <a href="#">Resources</a>
+                <button className="rounded-full btn-cta">Sing Up</button>
               </li>
             </ul>
-          </nav>
+          </div>
+          {isOpen && (
+            <div className="absolute text-center rounded-lg md:flex md:items-center top-16 right-5 left-5 md:text-left bg-violet-900 text-slate-100 md:relative md:top-0 md:right-0 md:left-0 md:bg-transparent md:text-slate-700 md:py-0">
+              <nav className="md:hidden md:justify-start md:items-start">
+                <ul className="py-4 space-y-6 md:flex md:space-x-5">
+                  <li className="text-xl font-bold tracking-wide">
+                    <a href="#">Features</a>
+                  </li>
+                  <li className="my-5 text-xl font-bold tracking-wide md:my-0">
+                    <a href="#">Pricing</a>
+                  </li>
+                  <li className="text-xl font-bold tracking-wide">
+                    <a href="#">Resources</a>
+                  </li>
+                </ul>
+              </nav>
+              <ul className="flex flex-col py-4 space-y-3 items-enter">
+                <li className="my-2 text-xl font-bold tracking-wide md:my-0">
+                  <button>Login</button>
+                </li>
+                <li>
+                  <button className="text-lg font-bold tracking-wide rounded-full btn-cta">
+                    Sing Up
+                  </button>
+                </li>
+              </ul>
+            </div>
+          )}
+          <button onClick={handleClick} className="md:hidden">
+            <img src={isOpen ? { menu } : { close }} />
+            {/* {isOpen ? "menu" : "close"} */}
+          </button>
         </div>
-        <ul className="">
-          <li>
-            <button>Login</button>
-          </li>
-          <li>
-            <button className="rounded-full btn-cta">Sing Up</button>
-          </li>
-        </ul>
       </header>
     </>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero() {
     <>
       <section className="py-10 lg:py-20">
         <div className="grid grid-cols-1 gap-5 max-width md:grid-cols-2 md:gap-10 md:place-items-center">
-          <article className="text-center md:text-left">
+          <article className="order-2 text-center md:text-left md:order-1">
             <h1 className="mb-6 text-5xl font-bold lg:text-6xl text-slate-800">
               More than just shorter links
             </h1>
@@ -14,7 +14,7 @@ export default function Hero() {
             </p>
             <button className="rounded-full btn-cta">Get Started</button>
           </article>
-          <article>
+          <article className="order-1 md:order-2">
             <img src={imageHero} />
           </article>
         </div>
